3c1ba5dad57b1bbb2ecbaffdf523f1218bb25040
Luis Gustavo <dekmis@hotmail.com>
2023-06-21 16:25:02 -0300

    all decorators - end

---
+++ src/A0057-decorators/A0056-decorator-params.ts
@@ -0,0 +1,43 @@
+function decorador(classPrototype: any, nome: string | symbol): any {
+  let valorPropriedade: any;
+  return {
+    get: () => valorPropriedade,
+    set: (valor: any) => {
+      valorPropriedade = valor.split('').reverse().join('');
+    },
+  };
+}
+
+export class UmaPessoa {
+  @decorador
+  nome: string;
+  sobrenome: string;
+  idade: number;
+
+  constructor(nome: string, sobrenome: string, idade: number) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+  }
+
+  metodo(msg: string): string {
+    return `${this.nome} ${this.sobrenome}: ${msg}`;
+  }
+
+  get nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\s+/g);
+    const primeiroNome = palavras.shift();
+    if (!primeiroNome) return;
+    this.nome = primeiroNome;
+    this.sobrenome = palavras.join(' ');
+  }
+}
+
+const pessoa = new UmaPessoa('Luis', 'Gustavo', 19);
+//pessoa.metodo = () => 'EEEEEEIII';
+const metodo = pessoa.metodo('ola mundo');
+console.log(metodo);
